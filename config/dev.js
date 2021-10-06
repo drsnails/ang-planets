323ab11ac7d8ef1e69cb2d269c8b0af1e6df3bad
Stav Yaar Bar <anistu@gmail.com>
2021-10-06 22:52:21 +0300

    a

--- config/dev.js
+++ config/dev.js
@@ -1,3 +1,4 @@
 module.exports = {
-  'dbURL': 'mongodb://localhost:27017',
+  // 'dbURL': 'mongodb://localhost:27017',
+  'dbURL': 'mongodb+srv://drsnails:Kexurhbu89@cluster0.jmn2v.mongodb.net/planet_db?retryWrites=true&w=majority',
 }
--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-  'dbURL': 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  'dbURL': 'mongodb+srv://drsnails:Kexurhbu89@cluster0.jmn2v.mongodb.net/planet_db?retryWrites=true&w=majority',
 }
